e50145663a1664bd821b9e6f566e261994f6f3f8
marian brinza <branza.marian03@yahoo.com>
2020-02-18 23:30:44 +0200

    add filter component

---
+++ src/components/filter/filter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './filter.module.scss';
+
+const Filter = (props) => {
+
+  return (
+    <div className={styles.filterComp}>
+      <label>
+        inline
+        <input type='checkbox' />
+      </label>
+      <label>
+        block
+        <input type='checkbox' />
+      </label>
+      <label>
+        html 5
+        <input type='checkbox' />
+      </label>
+      <label>
+        lists
+        <input type='checkbox' />
+      </label>
+    </div>
+  );
+};
+
+export default Filter;
