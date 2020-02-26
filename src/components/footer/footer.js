1b0816a4e5054146555575d51351d55f8bd9b955
marian brinza <branza.marian03@yahoo.com>
2020-02-26 23:18:46 +0200

    footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,9 +4,15 @@ import styles from './footer.module.scss';
 const Footer = (props) => {
 
   return (
-    <div className={styles.footerComp}>
-
-    </div>
+    <footer className={styles.footerComp}>
+      <div>
+        <p>made by © noAdsApps</p>
+        <p>No ads, no tracking & forever free</p>
+        <p>Website source code available at</p>
+        <p>github link here</p>
+        <p>Want to support my work or buy me a <span role="img" aria-label="beer emoji">🍺 </span></p>
+      </div>
+    </footer>
   );
 };
 
