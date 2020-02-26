f2f70f33d00fe919260848a8da27731b009310cb
marian brinza <branza.marian03@yahoo.com>
2020-02-26 23:17:35 +0200

    add constants file

---
+++ src/util/constants.js
@@ -0,0 +1,470 @@
+export const MyHtmlTags = [
+  {
+    name: '<a>',
+    type: 'inline',
+  },
+  {
+    name: '<abbr>',
+    type: 'inline',
+  },
+  {
+    name: '<address>',
+    type: 'inline',
+  },
+  {
+    name: '<area>',
+    type: 'inline',
+  },
+  {
+    name: '<article>',
+    type: 'inline',
+  },
+  {
+    name: '<aside>',
+    type: 'inline',
+  },
+  {
+    name: '<audio>',
+    type: 'inline',
+  },
+  {
+    name: '<b>',
+    type: 'inline',
+  },
+  {
+    name: '<base>',
+    type: 'inline',
+  },
+  {
+    name: '<bdi>',
+    type: 'inline',
+  },
+  {
+    name: '<bdo>',
+    type: 'inline',
+  },
+  {
+    name: '<blockquote>',
+    type: 'inline',
+  },
+  {
+    name: '<body>',
+    type: 'inline',
+  },
+  {
+    name: '<br>',
+    type: 'inline',
+  },
+  {
+    name: '<button>',
+    type: 'inline',
+  },
+  {
+    name: '<canvas>',
+    type: 'inline',
+  },
+  {
+    name: '<caption>',
+    type: 'inline',
+  },
+  {
+    name: '<cite>',
+    type: 'inline',
+  },
+  {
+    name: '<code>',
+    type: 'inline',
+  },
+  {
+    name: '<col>',
+    type: 'inline',
+  },
+  {
+    name: '<colgroup>',
+    type: 'inline',
+  },
+  {
+    name: '<data>',
+    type: 'inline',
+  },
+  {
+    name: '<datalist>',
+    type: 'inline',
+  },
+  {
+    name: '<dd>',
+    type: 'inline',
+  },
+  {
+    name: '<del>',
+    type: 'inline',
+  },
+  {
+    name: '<details>',
+    type: 'inline',
+  },
+  {
+    name: '<dfn>',
+    type: 'inline',
+  },
+  {
+    name: '<dialog>',
+    type: 'inline',
+  },
+  {
+    name: '<div>',
+    type: 'inline',
+  },
+  {
+    name: '<dl>',
+    type: 'inline',
+  },
+  {
+    name: '<dt>',
+    type: 'inline',
+  },
+  {
+    name: '<em>',
+    type: 'inline',
+  },
+  {
+    name: '<embed>',
+    type: 'inline',
+  },
+  {
+    name: '<fieldset>',
+    type: 'inline',
+  },
+  {
+    name: '<figcaption>',
+    type: 'inline',
+  },
+  {
+    name: '<figure>',
+    type: 'inline',
+  },
+  {
+    name: '<footer>',
+    type: 'inline',
+  },
+  {
+    name: '<form>',
+    type: 'inline',
+  },
+  {
+    name: '<h1>',
+    type: 'inline',
+  },
+  {
+    name: '<h2>',
+    type: 'inline',
+  },
+  {
+    name: '<h3>',
+    type: 'inline',
+  },
+  {
+    name: '<h4>',
+    type: 'inline',
+  },
+  {
+    name: '<h5>',
+    type: 'inline',
+  },
+  {
+    name: '<h6>',
+    type: 'inline',
+  },
+  {
+    name: '<head>',
+    type: 'inline',
+  },
+  {
+    name: '<header>',
+    type: 'inline',
+  },
+  {
+    name: '<hgroup>',
+    type: 'inline',
+  },
+  {
+    name: '<hr>',
+    type: 'inline',
+  },
+  {
+    name: '<html>',
+    type: 'inline',
+  },
+  {
+    name: '<i>',
+    type: 'inline',
+  },
+  {
+    name: '<iframe>',
+    type: 'inline',
+  },
+  {
+    name: '<img>',
+    type: 'inline',
+  },
+  {
+    name: '<input>',
+    type: 'inline',
+  },
+  {
+    name: '<ins>',
+    type: 'inline',
+  },
+  {
+    name: '<kbd>',
+    type: 'inline',
+  },
+  {
+    name: '<label>',
+    type: 'inline',
+  },
+  {
+    name: '<legend>',
+    type: 'inline',
+  },
+  {
+    name: '<li>',
+    type: 'inline',
+  },
+  {
+    name: '<link>',
+    type: 'inline',
+  },
+  {
+    name: '<main>',
+    type: 'inline',
+  },
+  {
+    name: '<map>',
+    type: 'inline',
+  },
+  {
+    name: '<mark>',
+    type: 'inline',
+  },
+  {
+    name: '<math>',
+    type: 'inline',
+  },
+  {
+    name: '<menu>',
+    type: 'inline',
+  },
+  {
+    name: '<menuitem>',
+    type: 'inline',
+  },
+  {
+    name: '<meta>',
+    type: 'inline',
+  },
+  {
+    name: '<meter>',
+    type: 'inline',
+  },
+  {
+    name: '<nav>',
+    type: 'inline',
+  },
+  {
+    name: '<noscript>',
+    type: 'inline',
+  },
+  {
+    name: '<object>',
+    type: 'inline',
+  },
+  {
+    name: '<ol>',
+    type: 'inline',
+  },
+  {
+    name: '<optgroup>',
+    type: 'inline',
+  },
+  {
+    name: '<option>',
+    type: 'inline',
+  },
+  {
+    name: '<output>',
+    type: 'inline',
+  },
+  {
+    name: '<p>',
+    type: 'inline',
+  },
+  {
+    name: '<param>',
+    type: 'inline',
+  },
+  {
+    name: '<picture>',
+    type: 'inline',
+  },
+  {
+    name: '<pre>',
+    type: 'inline',
+  },
+  {
+    name: '<progress>',
+    type: 'inline',
+  },
+  {
+    name: '<q>',
+    type: 'inline',
+  },
+  {
+    name: '<rb>',
+    type: 'inline',
+  },
+  {
+    name: '<rp>',
+    type: 'inline',
+  },
+  {
+    name: '<rt>',
+    type: 'inline',
+  },
+  {
+    name: '<rtc>',
+    type: 'inline',
+  },
+  {
+    name: '<ruby>',
+    type: 'inline',
+  },
+  {
+    name: '<s>',
+    type: 'inline',
+  },
+  {
+    name: '<samp>',
+    type: 'inline',
+  },
+  {
+    name: '<script>',
+    type: 'inline',
+  },
+  {
+    name: '<section>',
+    type: 'inline',
+  },
+  {
+    name: '<select>',
+    type: 'inline',
+  },
+  {
+    name: '<slot>',
+    type: 'inline',
+  },
+  {
+    name: '<small>',
+    type: 'inline',
+  },
+  {
+    name: '<source>',
+    type: 'inline',
+  },
+  {
+    name: '<span>',
+    type: 'inline',
+  },
+  {
+    name: '<strong>',
+    type: 'inline',
+  },
+  {
+    name: '<style>',
+    type: 'inline',
+  },
+  {
+    name: '<sub>',
+    type: 'inline',
+  },
+  {
+    name: '<summary>',
+    type: 'inline',
+  },
+  {
+    name: '<sup>',
+    type: 'inline',
+  },
+  {
+    name: '<svg>',
+    type: 'inline',
+  },
+  {
+    name: '<table>',
+    type: 'inline',
+  },
+  {
+    name: '<tbody>',
+    type: 'inline',
+  },
+  {
+    name: '<td>',
+    type: 'inline',
+  },
+  {
+    name: '<template>',
+    type: 'inline',
+  },
+  {
+    name: '<textarea>',
+    type: 'inline',
+  },
+  {
+    name: '<tfoot>',
+    type: 'inline',
+  },
+  {
+    name: '<th>',
+    type: 'inline',
+  },
+  {
+    name: '<thead>',
+    type: 'inline',
+  },
+  {
+    name: '<time>',
+    type: 'inline',
+  },
+  {
+    name: '<title>',
+    type: 'inline',
+  },
+  {
+    name: '<tr>',
+    type: 'inline',
+  },
+  {
+    name: '<track>',
+    type: 'inline',
+  },
+  {
+    name: '<u>',
+    type: 'inline',
+  },
+  {
+    name: '<ul>',
+    type: 'inline',
+  },
+  {
+    name: '<var>',
+    type: 'inline',
+  },
+  {
+    name: '<video>',
+    type: 'inline',
+  },
+  {
+    name: '<wbr>',
+    type: 'inline',
+  },
+];
