451f41b75dc770ac9c8ded1993848275ef3b3b2d
marian brinza <branza.marian03@yahoo.com>
2020-06-04 10:04:03 +0300

    add html5 property

--- src/resources/constants.js
+++ src/resources/constants.js
@@ -2,469 +2,586 @@ export const MyHtmlTags = [
   {
     name: '<a>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<abbr>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<address>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<area>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<article>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<aside>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<audio>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<b>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<base>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<bdi>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<bdo>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<blockquote>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<body>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<br>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<button>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<canvas>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<caption>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<cite>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<code>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<col>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<colgroup>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<data>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<datalist>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<dd>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<del>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<details>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<dfn>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<dialog>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<div>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<dl>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<dt>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<em>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<embed>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<fieldset>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<figcaption>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<figure>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<footer>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<form>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<h1>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<h2>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<h3>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<h4>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<h5>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<h6>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<head>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<header>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<hgroup>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<hr>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<html>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<i>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<iframe>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<img>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<input>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<ins>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<kbd>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<label>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<legend>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<li>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<link>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<main>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<map>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<mark>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<math>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<menu>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<menuitem>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<meta>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<meter>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<nav>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<noscript>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<object>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<ol>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<optgroup>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<option>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<output>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<p>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<param>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<picture>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<pre>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<progress>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<q>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<rb>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<rp>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<rt>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<rtc>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<ruby>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<s>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<samp>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<script>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<section>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<select>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<slot>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<small>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<source>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<span>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<strong>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<style>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<sub>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<summary>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<sup>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<svg>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<table>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<tbody>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<td>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<template>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<textarea>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<tfoot>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<th>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<thead>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<time>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<title>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<tr>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<track>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<u>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<ul>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<var>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<video>',
     type: 'inline',
+    html5: 'true'
   },
   {
     name: '<wbr>',
     type: 'inline',
+    html5: 'true'
   },
 ];
