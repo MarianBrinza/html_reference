a205f9283fcbde45c27c4c4948a548c3c36bb901
marian brinza <branza.marian03@yahoo.com>
2020-06-07 21:25:10 +0300

    use context

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import styles from './search.module.scss';
+import { SearchContext } from '../../context/SearchContext';
 
-const Search = (props) => {
-  const { placeholder = '' } = props;
+const Search = ({ placeholder = ''}) => {
+
+  const [searchValue, setSearchValue] = useState('');
+  const { setUserQuery } = useContext(SearchContext);
 
   return (
     <div className={styles.searchComp}>
-      <input type='text' placeholder={placeholder}/>
+      <input
+        type='text'
+        placeholder={placeholder}
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+          setUserQuery(e.target.value);
+        }}
+      />
     </div>
   );
 };
