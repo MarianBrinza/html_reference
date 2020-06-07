325c7217a48a3ecabe4e3102d2f20cb4b6273366
marian brinza <branza.marian03@yahoo.com>
2020-06-07 21:21:00 +0300

    add search and filter context

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo/seo';
-import TagTable from '../components/tagList/TagTable';
+import TagTable from '../components/tagTable/TagTable';
 import Search from '../components/search/search';
 import Filter from '../components/filter/filter';
+import SearchContextProvider from '../context/SearchContext';
+import GetAllHtmlElements from '../hooks/GetAllHtmlElements';
+import FilterContextProvider from '../context/FilterContext';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home"/>
-    <Search/>
-    <Filter/>
-    <TagTable/>
+const IndexPage = () => {
+  const htmlElementsToDisplay = GetAllHtmlElements();
 
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+  return (
+    <Layout>
+      <SEO title="Home"/>
+
+      <SearchContextProvider>
+        <Search placeholder='Search HTML element'/>
+        <FilterContextProvider>
+          <Filter/>
+          <TagTable htmlElementsToDisplay={htmlElementsToDisplay}/>
+        </FilterContextProvider>
+      </SearchContextProvider>
+
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
 
 export default IndexPage;
