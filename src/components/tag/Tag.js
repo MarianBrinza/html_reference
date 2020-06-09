9318d38603cd98ab9c90c7d7e3d06e6d82b2b2bb
marian brinza <branza.marian03@yahoo.com>
2020-06-09 16:50:37 +0300

    add self closing tags to list

--- src/components/tag/Tag.js
+++ src/components/tag/Tag.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import styles from './Tag.module.scss';
 
-const Tag = ({ tagName, tagType, html5 }) => {
+const Tag = ({ element }) => {
+  const { name, inline, html5, selfClosing } = element;
+  let tagType;
+
+  if (JSON.parse(inline)) {
+    tagType = 'inline';
+  } else {
+    tagType = 'block';
+  }
 
   return (
     <div className={styles.tagRow}>
       <div>
-        <span>{tagName}</span>
+        <span>{name}</span>
       </div>
       <div>
         <span>{tagType}</span>
       </div>
       <div>
         <span>{html5}</span>
+      </div>
+      <div>
+        <span>{selfClosing}</span>
       </div>
     </div>
   );
