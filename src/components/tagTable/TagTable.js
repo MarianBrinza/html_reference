d654dbaa103d5899b4ee3715c981f2f5950d9240
marian brinza <branza.marian03@yahoo.com>
2020-06-09 18:35:56 +0300

    add table header and filter functionality

--- src/components/tagTable/TagTable.js
+++ src/components/tagTable/TagTable.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './TagTable.module.scss';
 import Tag from '../tag/Tag';
+import { SearchContext } from '../../context/SearchContext';
+import { FilterContext } from '../../context/FilterContext';
+import { getDisplayElements, getStringFilters } from '../../resources/util';
 
 const TagTable = ({ htmlElementsToDisplay }) => {
+  const { userQuery } = useContext(SearchContext);
+  const { filters } = useContext(FilterContext);
+
+  const elements = userQuery.length > 0 ? htmlElementsToDisplay.filter(el => el.name.includes(userQuery)) : htmlElementsToDisplay;
+  const trueFilters = getStringFilters(filters);
+  const trueElements = getDisplayElements(elements, trueFilters);
+
+  let displayElements = [];
+  if (trueFilters) {
+    displayElements = trueElements;
+  } else {
+    displayElements = elements;
+  }
 
   return (
     <div className={styles.tagListComp}>
+      <div className={styles.tableHeader}>
+        <div>Tag</div>
+        <div>inline / block</div>
+        <div>html5</div>
+        <div>self-closing</div>
+      </div>
       {
-        htmlElementsToDisplay.map((file, index) => {
+        displayElements.map(element => {
           return (
             <Tag
-              tagName={file.node.frontmatter.name}
-              tagType={file.node.frontmatter.type}
-              html5={file.node.frontmatter.html5}
-              key={file.node.frontmatter.name}
-            >
+              element={element}
+              key={element.name}>
             </Tag>
           );
         })
