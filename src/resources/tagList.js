4faeeedd3c10ba190381b9617c576fd8c240c124
marian <branza.marian03@yahoo.com>
2021-01-10 11:16:55 +0200

    tag list with short description

---
+++ src/resources/tagList.js
@@ -0,0 +1,537 @@
+export const aa = [
+  {
+    "name": "<!--...-->",
+    "url": "https://www.w3schools.com/tags/tag_comment.asp",
+    "description": "Defines a comment"
+  },
+  {
+    "name": "<!DOCTYPE>",
+    "url": "https://www.w3schools.com/tags/tag_doctype.asp",
+    "description": "Defines the document type"
+  },
+  {
+    "name": "<a>",
+    "url": "https://www.w3schools.com/tags/tag_a.asp",
+    "description": "Defines a hyperlink"
+  },
+  {
+    "name": "<abbr>",
+    "url": "https://www.w3schools.com/tags/tag_abbr.asp",
+    "description": "Defines an abbreviation or an acronym"
+  },
+  {
+    "name": "<address>",
+    "url": "https://www.w3schools.com/tags/tag_address.asp",
+    "description": "Defines contact information for the author/owner of a document"
+  },
+  {
+    "name": "<area>",
+    "url": "https://www.w3schools.com/tags/tag_area.asp",
+    "description": "Defines an area inside an image map"
+  },
+  {
+    "name": "<article>",
+    "url": "https://www.w3schools.com/tags/tag_article.asp",
+    "description": "Defines an article"
+  },
+  {
+    "name": "<aside>",
+    "url": "https://www.w3schools.com/tags/tag_aside.asp",
+    "description": "Defines content aside from the page content"
+  },
+  {
+    "name": "<audio>",
+    "url": "https://www.w3schools.com/tags/tag_audio.asp",
+    "description": "Defines embedded sound content"
+  },
+  {
+    "name": "<b>",
+    "url": "https://www.w3schools.com/tags/tag_b.asp",
+    "description": "Defines bold text"
+  },
+  {
+    "name": "<base>",
+    "url": "https://www.w3schools.com/tags/tag_base.asp",
+    "description": "Specifies the base URL/target for all relative URLs in a document"
+  },
+  {
+    "name": "<bdi>",
+    "url": "https://www.w3schools.com/tags/tag_bdi.asp",
+    "description": "Isolates a part of text that might be formatted in a different direction from other text outside it"
+  },
+  {
+    "name": "<bdo>",
+    "url": "https://www.w3schools.com/tags/tag_bdo.asp",
+    "description": "Overrides the current text direction"
+  },
+  {
+    "name": "<blockquote>",
+    "url": "https://www.w3schools.com/tags/tag_blockquote.asp",
+    "description": "Defines a section that is quoted from another source"
+  },
+  {
+    "name": "<body>",
+    "url": "https://www.w3schools.com/tags/tag_body.asp",
+    "description": "Defines the document's body"
+  },
+  {
+    "name": "<br>",
+    "url": "https://www.w3schools.com/tags/tag_br.asp",
+    "description": "Defines a single line break"
+  },
+  {
+    "name": "<button>",
+    "url": "https://www.w3schools.com/tags/tag_button.asp",
+    "description": "Defines a clickable button"
+  },
+  {
+    "name": "<canvas>",
+    "url": "https://www.w3schools.com/tags/tag_canvas.asp",
+    "description": "Used to draw graphics, on the fly, via scripting (usually JavaScript)"
+  },
+  {
+    "name": "<caption>",
+    "url": "https://www.w3schools.com/tags/tag_caption.asp",
+    "description": "Defines a table caption"
+  },
+  {
+    "name": "<cite>",
+    "url": "https://www.w3schools.com/tags/tag_cite.asp",
+    "description": "Defines the title of a work"
+  },
+  {
+    "name": "<code>",
+    "url": "https://www.w3schools.com/tags/tag_code.asp",
+    "description": "Defines a piece of computer code"
+  },
+  {
+    "name": "<col>",
+    "url": "https://www.w3schools.com/tags/tag_col.asp",
+    "description": "Specifies column properties for each column within a <colgroup> element"
+  },
+  {
+    "name": "<colgroup>",
+    "url": "https://www.w3schools.com/tags/tag_colgroup.asp",
+    "description": "Specifies a group of one or more columns in a table for formatting"
+  },
+  {
+    "name": "<data>",
+    "url": "https://www.w3schools.com/tags/tag_data.asp",
+    "description": "Adds a machine-readable translation of a given content"
+  },
+  {
+    "name": "<datalist>",
+    "url": "https://www.w3schools.com/tags/tag_datalist.asp",
+    "description": "Specifies a list of pre-defined options for input controls"
+  },
+  {
+    "name": "<dd>",
+    "url": "https://www.w3schools.com/tags/tag_dd.asp",
+    "description": "Defines a description/value of a term in a description list"
+  },
+  {
+    "name": "<del>",
+    "url": "https://www.w3schools.com/tags/tag_del.asp",
+    "description": "Defines text that has been deleted from a document"
+  },
+  {
+    "name": "<details>",
+    "url": "https://www.w3schools.com/tags/tag_details.asp",
+    "description": "Defines additional details that the user can view or hide"
+  },
+  {
+    "name": "<dfn>",
+    "url": "https://www.w3schools.com/tags/tag_dfn.asp",
+    "description": "Specifies a term that is going to be defined within the content"
+  },
+  {
+    "name": "<dialog>",
+    "url": "https://www.w3schools.com/tags/tag_dialog.asp",
+    "description": "Defines a dialog box or window"
+  },
+  {
+    "name": "<div>",
+    "url": "https://www.w3schools.com/tags/tag_div.asp",
+    "description": "Defines a section in a document"
+  },
+  {
+    "name": "<dl>",
+    "url": "https://www.w3schools.com/tags/tag_dl.asp",
+    "description": "Defines a description list"
+  },
+  {
+    "name": "<dt>",
+    "url": "https://www.w3schools.com/tags/tag_dt.asp",
+    "description": "Defines a term/name in a description list"
+  },
+  {
+    "name": "<em>",
+    "url": "https://www.w3schools.com/tags/tag_em.asp",
+    "description": "Defines emphasized text"
+  },
+  {
+    "name": "<embed>",
+    "url": "https://www.w3schools.com/tags/tag_embed.asp",
+    "description": "Defines a container for an external application"
+  },
+  {
+    "name": "<fieldset>",
+    "url": "https://www.w3schools.com/tags/tag_fieldset.asp",
+    "description": "Groups related elements in a form"
+  },
+  {
+    "name": "<figcaption>",
+    "url": "https://www.w3schools.com/tags/tag_figcaption.asp",
+    "description": "Defines a caption for a <figure> element"
+  },
+  {
+    "name": "<figure>",
+    "url": "https://www.w3schools.com/tags/tag_figure.asp",
+    "description": "Specifies self-contained content"
+  },
+  {
+    "name": "<footer>",
+    "url": "https://www.w3schools.com/tags/tag_footer.asp",
+    "description": "Defines a footer for a document or section"
+  },
+  {
+    "name": "<form>",
+    "url": "https://www.w3schools.com/tags/tag_form.asp",
+    "description": "Defines an HTML form for user input"
+  },
+  {
+    "name": "<h1> to <h6>",
+    "url": "https://www.w3schools.com/tags/tag_hn.asp",
+    "description": " Defines HTML headings"
+  },
+  {
+    "name": "<head>",
+    "url": "https://www.w3schools.com/tags/tag_head.asp",
+    "description": "Contains metadata/information for the document"
+  },
+  {
+    "name": "<header>",
+    "url": "https://www.w3schools.com/tags/tag_header.asp",
+    "description": "Defines a header for a document or section"
+  },
+  {
+    "name": "<hr>",
+    "url": "https://www.w3schools.com/tags/tag_hr.asp",
+    "description": " Defines a thematic change in the content"
+  },
+  {
+    "name": "<html>",
+    "url": "https://www.w3schools.com/tags/tag_html.asp",
+    "description": "Defines the root of an HTML document"
+  },
+  {
+    "name": "<i>",
+    "url": "https://www.w3schools.com/tags/tag_i.asp",
+    "description": "Defines a part of text in an alternate voice or mood"
+  },
+  {
+    "name": "<iframe>",
+    "url": "https://www.w3schools.com/tags/tag_iframe.asp",
+    "description": "Defines an inline frame"
+  },
+  {
+    "name": "<img>",
+    "url": "https://www.w3schools.com/tags/tag_img.asp",
+    "description": "Defines an image"
+  },
+  {
+    "name": "<input>",
+    "url": "https://www.w3schools.com/tags/tag_input.asp",
+    "description": "Defines an input control"
+  },
+  {
+    "name": "<ins>",
+    "url": "https://www.w3schools.com/tags/tag_ins.asp",
+    "description": "Defines a text that has been inserted into a document"
+  },
+  {
+    "name": "<kbd>",
+    "url": "https://www.w3schools.com/tags/tag_kbd.asp",
+    "description": "Defines keyboard input"
+  },
+  {
+    "name": "<label>",
+    "url": "https://www.w3schools.com/tags/tag_label.asp",
+    "description": "Defines a label for an <input> element"
+  },
+  {
+    "name": "<legend>",
+    "url": "https://www.w3schools.com/tags/tag_legend.asp",
+    "description": "Defines a caption for a <fieldset> element"
+  },
+  {
+    "name": "<li>",
+    "url": "https://www.w3schools.com/tags/tag_li.asp",
+    "description": "Defines a list item"
+  },
+  {
+    "name": "<link>",
+    "url": "https://www.w3schools.com/tags/tag_link.asp",
+    "description": "Defines the relationship between a document and an external resource (most used to link to style sheets)"
+  },
+  {
+    "name": "<main>",
+    "url": "https://www.w3schools.com/tags/tag_main.asp",
+    "description": "Specifies the main content of a document"
+  },
+  {
+    "name": "<map>",
+    "url": "https://www.w3schools.com/tags/tag_map.asp",
+    "description": "Defines an image map"
+  },
+  {
+    "name": "<mark>",
+    "url": "https://www.w3schools.com/tags/tag_mark.asp",
+    "description": "Defines marked/highlighted text"
+  },
+  {
+    "name": "<meta>",
+    "url": "https://www.w3schools.com/tags/tag_meta.asp",
+    "description": "Defines metadata about an HTML document"
+  },
+  {
+    "name": "<meter>",
+    "url": "https://www.w3schools.com/tags/tag_meter.asp",
+    "description": "Defines a scalar measurement within a known range (a gauge)"
+  },
+  {
+    "name": "<nav>",
+    "url": "https://www.w3schools.com/tags/tag_nav.asp",
+    "description": "Defines navigation links"
+  },
+  {
+    "name": "<noscript>",
+    "url": "https://www.w3schools.com/tags/tag_noscript.asp",
+    "description": "Defines an alternate content for users that do not support client-side scripts"
+  },
+  {
+    "name": "<object>",
+    "url": "https://www.w3schools.com/tags/tag_object.asp",
+    "description": "Defines a container for an external application"
+  },
+  {
+    "name": "<ol>",
+    "url": "https://www.w3schools.com/tags/tag_ol.asp",
+    "description": "Defines an ordered list"
+  },
+  {
+    "name": "<optgroup>",
+    "url": "https://www.w3schools.com/tags/tag_optgroup.asp",
+    "description": "Defines a group of related options in a drop-down list"
+  },
+  {
+    "name": "<option>",
+    "url": "https://www.w3schools.com/tags/tag_option.asp",
+    "description": "Defines an option in a drop-down list"
+  },
+  {
+    "name": "<output>",
+    "url": "https://www.w3schools.com/tags/tag_output.asp",
+    "description": "Defines the result of a calculation"
+  },
+  {
+    "name": "<p>",
+    "url": "https://www.w3schools.com/tags/tag_p.asp",
+    "description": "Defines a paragraph"
+  },
+  {
+    "name": "<param>",
+    "url": "https://www.w3schools.com/tags/tag_param.asp",
+    "description": "Defines a parameter for an object"
+  },
+  {
+    "name": "<picture>",
+    "url": "https://www.w3schools.com/tags/tag_picture.asp",
+    "description": "Defines a container for multiple image resources"
+  },
+  {
+    "name": "<pre>",
+    "url": "https://www.w3schools.com/tags/tag_pre.asp",
+    "description": "Defines preformatted text"
+  },
+  {
+    "name": "<progress>",
+    "url": "https://www.w3schools.com/tags/tag_progress.asp",
+    "description": "Represents the progress of a task"
+  },
+  {
+    "name": "<q>",
+    "url": "https://www.w3schools.com/tags/tag_q.asp",
+    "description": "Defines a short quotation"
+  },
+  {
+    "name": "<rp>",
+    "url": "https://www.w3schools.com/tags/tag_rp.asp",
+    "description": "Defines what to show in browsers that do not support ruby annotations"
+  },
+  {
+    "name": "<rt>",
+    "url": "https://www.w3schools.com/tags/tag_rt.asp",
+    "description": "Defines an explanation/pronunciation of characters (for East Asian typography)"
+  },
+  {
+    "name": "<ruby>",
+    "url": "https://www.w3schools.com/tags/tag_ruby.asp",
+    "description": "Defines a ruby annotation (for East Asian typography)"
+  },
+  {
+    "name": "<s>",
+    "url": "https://www.w3schools.com/tags/tag_s.asp",
+    "description": "Defines text that is no longer correct"
+  },
+  {
+    "name": "<samp>",
+    "url": "https://www.w3schools.com/tags/tag_samp.asp",
+    "description": "Defines sample output from a computer program"
+  },
+  {
+    "name": "<script>",
+    "url": "https://www.w3schools.com/tags/tag_script.asp",
+    "description": "Defines a client-side script"
+  },
+  {
+    "name": "<section>",
+    "url": "https://www.w3schools.com/tags/tag_section.asp",
+    "description": "Defines a section in a document"
+  },
+  {
+    "name": "<select>",
+    "url": "https://www.w3schools.com/tags/tag_select.asp",
+    "description": "Defines a drop-down list"
+  },
+  {
+    "name": "<small>",
+    "url": "https://www.w3schools.com/tags/tag_small.asp",
+    "description": "Defines smaller text"
+  },
+  {
+    "name": "<source>",
+    "url": "https://www.w3schools.com/tags/tag_source.asp",
+    "description": "Defines multiple media resources for media elements (<video> and <audio>)"
+  },
+  {
+    "name": "<span>",
+    "url": "https://www.w3schools.com/tags/tag_span.asp",
+    "description": "Defines a section in a document"
+  },
+  {
+    "name": "<strong>",
+    "url": "https://www.w3schools.com/tags/tag_strong.asp",
+    "description": "Defines important text"
+  },
+  {
+    "name": "<style>",
+    "url": "https://www.w3schools.com/tags/tag_style.asp",
+    "description": "Defines style information for a document"
+  },
+  {
+    "name": "<sub>",
+    "url": "https://www.w3schools.com/tags/tag_sub.asp",
+    "description": "Defines subscripted text"
+  },
+  {
+    "name": "<summary>",
+    "url": "https://www.w3schools.com/tags/tag_summary.asp",
+    "description": "Defines a visible heading for a <details> element"
+  },
+  {
+    "name": "<sup>",
+    "url": "https://www.w3schools.com/tags/tag_sup.asp",
+    "description": "Defines superscripted text"
+  },
+  {
+    "name": "<svg>",
+    "url": "https://www.w3schools.com/tags/tag_svg.asp",
+    "description": "Defines a container for SVG graphics"
+  },
+  {
+    "name": "<table>",
+    "url": "https://www.w3schools.com/tags/tag_table.asp",
+    "description": "Defines a table"
+  },
+  {
+    "name": "<tbody>",
+    "url": "https://www.w3schools.com/tags/tag_tbody.asp",
+    "description": "Groups the body content in a table"
+  },
+  {
+    "name": "<td>",
+    "url": "https://www.w3schools.com/tags/tag_td.asp",
+    "description": "Defines a cell in a table"
+  },
+  {
+    "name": "<template>",
+    "url": "https://www.w3schools.com/tags/tag_template.asp",
+    "description": "Defines a container for content that should be hidden when the page loads"
+  },
+  {
+    "name": "<textarea>",
+    "url": "https://www.w3schools.com/tags/tag_textarea.asp",
+    "description": "Defines a multiline input control (text area)"
+  },
+  {
+    "name": "<tfoot>",
+    "url": "https://www.w3schools.com/tags/tag_tfoot.asp",
+    "description": "Groups the footer content in a table"
+  },
+  {
+    "name": "<th>",
+    "url": "https://www.w3schools.com/tags/tag_th.asp",
+    "description": "Defines a header cell in a table"
+  },
+  {
+    "name": "<thead>",
+    "url": "https://www.w3schools.com/tags/tag_thead.asp",
+    "description": "Groups the header content in a table"
+  },
+  {
+    "name": "<time>",
+    "url": "https://www.w3schools.com/tags/tag_time.asp",
+    "description": "Defines a specific time (or datetime)"
+  },
+  {
+    "name": "<title>",
+    "url": "https://www.w3schools.com/tags/tag_title.asp",
+    "description": "Defines a title for the document"
+  },
+  {
+    "name": "<tr>",
+    "url": "https://www.w3schools.com/tags/tag_tr.asp",
+    "description": "Defines a row in a table"
+  },
+  {
+    "name": "<track>",
+    "url": "https://www.w3schools.com/tags/tag_track.asp",
+    "description": "Defines text tracks for media elements (<video> and <audio>)"
+  },
+  {
+    "name": "<u>",
+    "url": "https://www.w3schools.com/tags/tag_u.asp",
+    "description": "Defines some text that is unarticulated and styled differently from normal text"
+  },
+  {
+    "name": "<ul>",
+    "url": "https://www.w3schools.com/tags/tag_ul.asp",
+    "description": "Defines an unordered list"
+  },
+  {
+    "name": "<var>",
+    "url": "https://www.w3schools.com/tags/tag_var.asp",
+    "description": "Defines a variable"
+  },
+  {
+    "name": "<video>",
+    "url": "https://www.w3schools.com/tags/tag_video.asp",
+    "description": "Defines embedded video content"
+  },
+  {
+    "name": "<wbr>",
+    "url": "https://www.w3schools.com/tags/tag_wbr.asp",
+    "description": "Defines a possible line-break"
+  }
+];
