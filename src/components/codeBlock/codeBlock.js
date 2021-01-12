e2ffce4d622111cde0c45122fb154451cf09167c
marian <branza.marian03@yahoo.com>
2021-01-12 23:47:11 +0200

    use styles

--- src/components/codeBlock/codeBlock.js
+++ src/components/codeBlock/codeBlock.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+import style from './codeBlock.module.scss';
 
 SyntaxHighlighter.registerLanguage('jsx', jsx);
 
