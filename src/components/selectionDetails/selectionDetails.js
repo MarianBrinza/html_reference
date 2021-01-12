f74805d938e3b77df5755af1ed8c18b318e2ddde
marian <branza.marian03@yahoo.com>
2021-01-12 17:43:18 +0200

    update

--- src/components/selectionDetails/selectionDetails.js
+++ src/components/selectionDetails/selectionDetails.js
@@ -3,6 +3,14 @@ import styles from './selectionDetails.module.scss';
 import { SelectedElementContext } from '../../context/SelectedElementContext';
 import { toggle } from '../../resources/util';
 
+//import the Prism package
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+
 const SelectionDetails = () => {
   let { selectedElement } = useContext(SelectedElementContext);
 
@@ -10,49 +18,99 @@ const SelectionDetails = () => {
   console.log(selectedElement);
   const {
     block,
+    defaultCssCode,
+    defaultCssText,
+    definition,
+    description,
+    exampleCode,
+    exampleText,
+    globalAttrib,
+    globalEvents,
     html5,
     inline,
     name,
+    nameDescription,
     selfClosing,
-    title,
-    description,
-    attribArr,
-    valuesArr
+    attributes
   } = selectedElement;
+
+  let parentWidth = document.querySelector('#selection-details');
+  if (parentWidth) {
+    parentWidth = (parentWidth.clientWidth - 20) + 'px';
+    console.log('selection-details width = ', parentWidth);
+  }
 
   return (
     <div id='selection-details' className={styles.selectionDetailsComp}>
-      {/*{Object.keys(selectedElement).length > 0 && (*/}
-      {/*  <>*/}
-      <button id={styles.toggleBtn}
-              onClick={() => {
-                toggle('#searchWrapper');
-                toggle('#elementsTable');
-                toggle('#selection-details');
-              }}
-      >
-        Close me!
-      </button>
+      {Object.keys(selectedElement).length > 0 && (
+        <>
+          {/* close button for mobile */}
+          <button id={styles.toggleBtn}
+                  onClick={() => {
+                    toggle('#searchWrapper');
+                    toggle('#elementsTable');
+                    toggle('#selection-details');
+                  }}
+          >
+            Close me!
+          </button>
 
-      {/*    <p>{block}</p>*/}
-      {/*    <p>{html5}</p>*/}
-      {/*    <p>{inline}</p>*/}
-      {/*    <p>{name}</p>*/}
-      {/*    <p>{selfClosing}</p>*/}
-      {/*  </>*/}
-      {/*)}*/}
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta dignissimos incidunt ipsa libero nobis nulla
-        placeat, qui quidem quisquam. Accusantium beatae cumque distinctio nesciunt obcaecati quae quia quo
-        recusandae?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta dignissimos incidunt ipsa libero nobis nulla
-        placeat, qui quidem quisquam. Accusantium beatae cumque distinctio nesciunt obcaecati quae quia quo
-        recusandae?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta dignissimos incidunt ipsa libero nobis nulla
-        placeat, qui quidem quisquam. Accusantium beatae cumque distinctio nesciunt obcaecati quae quia quo
-        recusandae?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta dignissimos incidunt ipsa libero nobis nulla
-        placeat, qui quidem quisquam. Accusantium beatae cumque distinctio nesciunt obcaecati quae quia quo
-        recusandae?</p>
+
+          <h1 className={styles.title}>The {name} tag</h1>
+
+
+          <p>Description: {description}</p>
+
+
+          <p>Example Code: {exampleText}</p>
+          <SyntaxHighlighter language='xml' style={materialDark}
+                             customStyle={{
+                               margin: '20px 0',
+                               borderRadius: '5px'
+                             }}>
+            {exampleCode}
+          </SyntaxHighlighter>
+
+          {/* default css style*/}
+          <p>{defaultCssText}</p>
+          <SyntaxHighlighter language='css' style={materialDark}>
+            {defaultCssCode}
+          </SyntaxHighlighter>
+
+
+          <h2>Definition and Usage</h2>
+          <p>{definition}</p>
+
+
+          <h2>Global attributes:</h2>
+          <p>{globalAttrib}</p>
+
+
+          <h2>Global events:</h2>
+          <p>{globalEvents}</p>
+
+          <table>
+            <thead>
+            <tr>
+              <th>Inline</th>
+              <th>Block</th>
+              <th>Html5</th>
+              <th>Self closing</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>{inline}</td>
+              <td>{block}</td>
+              <td>{html5}</td>
+              <td>{selfClosing}</td>
+            </tr>
+            </tbody>
+          </table>
+
+        </>
+      )}
+
     </div>
   );
 };
