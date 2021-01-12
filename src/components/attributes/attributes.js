b38a3c366cd210b63504ff172971940e07e2cd52
marian <branza.marian03@yahoo.com>
2021-01-12 23:33:31 +0200

    attributes component

---
+++ src/components/attributes/attributes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './attributes.module.scss';
+
+const Attributes = ({ attributes }) => {
+
+  console.log(attributes);
+  return (
+    <div className={styles.attributesComp}>
+
+
+      <section>
+        <table>
+          <thead>
+          <tr>
+            <th>name</th>
+            <th>value</th>
+            <th>description</th>
+          </tr>
+          </thead>
+          <tbody>
+          {attributes.map((el, index) => {
+            return (
+              <tr key={index}>
+                <td>{el.name}</td>
+                <td>{el.value}</td>
+                <td>{el.description}</td>
+              </tr>
+
+            );
+          })}
+          </tbody>
+        </table>
+      </section>
+    </div>);
+};
+
+export default Attributes;
