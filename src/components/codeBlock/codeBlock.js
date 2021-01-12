1a6eac265182a9a9beb77f2ce10d10a4ec2d6073
marian <branza.marian03@yahoo.com>
2021-01-12 18:53:42 +0200

    extract SyntaxHighlighter to it's own component

---
+++ src/components/codeBlock/codeBlock.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+
+const CodeBlock = ({ customStyle, exampleCode, language }) => {
+
+  return (<>
+
+    <SyntaxHighlighter language={language} style={materialDark}
+                       customStyle={customStyle}>
+      {exampleCode}
+    </SyntaxHighlighter>
+
+  </>);
+};
+
+export default CodeBlock;
